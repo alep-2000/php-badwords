bc426a561b497f9e5f7238fe169ad78620bd850f
alep-2000 <alessiopiras056@gmail.com>
2023-07-03 10:09:24 +0200

    Add structure html to visualize the paragraph

--- second.php
+++ second.php
@@ -4,9 +4,28 @@
 
   $paragraph = $_POST["paragraph"];
   
-  echo $paragraph. "  " .strlen($paragraph);
+  $startParagraph = $paragraph;
+  $lenghtParagraph = strlen($paragraph);
 
   $str = str_replace("$word", "***", $paragraph);
 
-  echo "<br> <br> <br> <br>" .$str. "  " .strlen($str);
+  $censoredParagraph = $str;
+  $lenghtCensored = strlen($censoredParagraph);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords</title>
+</head>
+<body>
+    <h3><strong>Paragrafo iniziale</strong></h3>
+    <p><?php echo $startParagraph ?></p>
+    <div><strong>Lunghezza paragrafo:</strong> <?php echo $lenghtParagraph ?></div>
+    <h3><strong>Paragrafo censurato</strong></h3>
+    <p><?php echo $censoredParagraph ?></p>
+    <div><strong>Lunghezza paragrafo:</strong> <?php echo $lenghtCensored ?></div>
+</body>
+</html>
